6a09b5e3c5b326eaa2d61eaf773cbb8ba73e1bca
Operations Research Engineering Software+ <alex@oresoftware.com>
2018-09-22 13:15:17 -0700

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 'use strict';
 
 import * as stream from 'stream';
+import {JSONParser} from '@oresoftware/json-stream-parser';
 
 //////////////////////////////////////////////////
-
 
 export interface IParsedObject {
   [index: string]: any
@@ -11,60 +11,18 @@ export interface IParsedObject {
 
 //////////////////////////////////////////////////
 
-export const createParser =  function () {
-
-  let lastLineData = '';
-
-  const strm = new stream.Transform({
-
-    objectMode: true,
-
-    transform(chunk: any, encoding: string, cb: Function) {
-
-      let data = String(chunk);
-      if (lastLineData) {
-        data = lastLineData + data;
-      }
-
-      let lines = data.split('\n');
-      lastLineData = lines.splice(lines.length - 1, 1)[0];
-
-      lines.forEach(l => {
-        try {
-          // l might be an empty string; ignore if so
-          l && this.push(JSON.parse(l));
-        }
-        catch (err) {
-          // noop
-        }
-      });
-
-      cb();
-
-    },
-
-    flush(cb: Function) {
-      if (lastLineData) {
-        try {
-          this.push(JSON.parse(lastLineData));
-        }
-        catch (err) {
-          // noop
-        }
-      }
-      lastLineData = '';
-      cb();
-    }
-  });
-
-  strm.on('data', function (d: IParsedObject) {
+export const createParser = () => {
+  
+  const strm = new JSONParser();
+  
+  strm.on('data', (d: IParsedObject) => {
     if (d && d['@tap-json'] === true) {
       strm.emit('testpoint', d);
     }
   });
-
+  
   return strm;
-
+  
 };
 
 export default createParser;
